c0b720d48142d06f602d45d78b009c7e46d2fdf9
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-05-15 18:56:04 +0200

    Empty project

---
+++ src/test/java/pl/xsware/XsWareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.xsware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsWareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
