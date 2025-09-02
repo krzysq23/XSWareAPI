7f0938b6842d923c3c71a52f113d590f7cac4a6a
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-09-02 13:00:14 +0200

    Dodanie this.percentSpent = percentSpent.intValue();

--- src/main/java/pl/xsware/domain/model/budget/BudgetLimit.java
+++ src/main/java/pl/xsware/domain/model/budget/BudgetLimit.java
@@ -37,6 +37,7 @@ public class BudgetLimit {
                 .multiply(new BigDecimal("100"))
                 .divide(amountLimit, 2, RoundingMode.HALF_UP);
         this.status = BudgetStatus.calculate(percentSpent);
+        this.percentSpent = percentSpent.intValue();
     }
 
 }
